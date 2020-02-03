89313ee2d523c76f6c340395663c463ea45ae799
agohorel <gohorelalex@gmail.com>
2020-02-03 17:44:26 -0500

    add register action

--- saltinator/src/reducers/rootReducer.js
+++ saltinator/src/reducers/rootReducer.js
@@ -13,7 +13,25 @@ export const rootReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    // case types.REGISTER_ST
+    case types.REGISTER_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case types.REGISTER_SUCCESS:
+      return {
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        errors: null
+      };
+    case types.REGISTER_FAILURE:
+      return {
+        ...state,
+        isAuthenticated: false,
+        isLoading: false,
+        errors: payload
+      };
     default:
       return {
         ...state

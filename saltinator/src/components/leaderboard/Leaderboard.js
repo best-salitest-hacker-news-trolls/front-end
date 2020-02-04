2760a3856fa8fc6ac8281ecb4b47ccb6bdd38d3e
unknown <jrhemann@gmail.com>
2020-02-04 11:13:00 -0600

    Added leaderboard

--- saltinator/src/components/leaderboard/Leaderboard.js
+++ saltinator/src/components/leaderboard/Leaderboard.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LeaderCard from "./LeaderCard";
+import { leaderboardData } from "../../utils/mockData";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 export const Leaderboard = () => {
-    return (
-        <div>
-            leaderboard
-        </div>
-    )
-}
+  const [leaders, setLeaders] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+
+      .then(response => {
+        setLeaders(response.data);
+        console.log(response.data);
+      })
+
+      .catch(error => {
+        console.log("Server Error", error);
+      });
+  }, []);
+  // const newLeader = Object.key(leaders)
+  return (
+    <div>
+      <form>
+        <h1>Leaderboard</h1>
+
+        <HeaderContainer>
+          <h2>Rank</h2>
+          <h2>Username</h2>
+          <h2>Comments</h2>
+        </HeaderContainer>
+      </form>
+      {leaderboardData.map((leader, index) => (
+        <LeaderCard leader={leader} key={index} />
+      ))}
+    </div>
+  );
+};

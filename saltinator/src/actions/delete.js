69d055b144401ffbf303c6b2baa4636adda8f58f
agohorel <gohorelalex@gmail.com>
2020-02-04 19:15:15 -0500

    add comments to delete action to clarify the funk-factor

--- saltinator/src/actions/delete.js
+++ saltinator/src/actions/delete.js
@@ -3,14 +3,17 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { DELETE_LOADING, DELETE_SUCCESS, DELETE_FAILURE } from "./types";
 
 export const deleteComment = (id, comment_id) => dispatch => {
-  console.log("firing!!!");
   dispatch({ type: DELETE_LOADING });
   return axiosWithAuth()
     .delete(`users/${id}/favorites/${comment_id}`)
     .then(res => {
+      // this grabs the index after the last slash in the request url (the one we just sent)
       const lastSlash = res.config.url.lastIndexOf("/") + 1;
       dispatch({
         type: DELETE_SUCCESS,
+        // below we're passing everything after the last slash - which is the ID of the comment we just deleted
+        // why? so we can filter out the deleted comment in the reducer
+        // this way we don't need to make an additional GET request to see our deletion reflected in the UI
         payload: res.config.url.substring(lastSlash)
       });
     })

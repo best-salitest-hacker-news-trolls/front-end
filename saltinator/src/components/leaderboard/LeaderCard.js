b3c5daada6e545c6e7d343179e1105c7fd44f658
agohorel <gohorelalex@gmail.com>
2020-02-06 16:29:41 -0500

    improve readability of leaderboard components

--- saltinator/src/components/leaderboard/LeaderCard.js
+++ saltinator/src/components/leaderboard/LeaderCard.js
@@ -2,27 +2,35 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import colors from "../../styles/colors";
+
 const Card = styled.div`
-  display: flex;
-  align-content: center;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: center;
   color: white;
   background-color: #97adce;
-  padding: 10px;
-  // border: 2px solid red;
-  margin: 5px;
+  padding: 2rem;
+  margin-bottom: 1.5rem;
   width: 45vw;
-    @media (max-width: 900px) {
-      width: 65vw;
-    }
-    @media (max-width: 500px) {
-      width: 95vw;
-    }
+  background-color: ${colors.card};
+  color: ${colors.text};
+  border-radius: 4px;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
+
+  @media (max-width: 900px) {
+    width: 65vw;
+  }
+  @media (max-width: 500px) {
+    width: 95vw;
+  }
 `;
 
 const Button = styled.button`
-  background-color: #2a3c58;
+  background-color: ${colors.background};
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
   color: white;
+  margin: 0;
   font-family: "Roboto Mono", monospace;
 
   :hover {
@@ -33,18 +41,35 @@ const Button = styled.button`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  justify-self: end;
 `;
 
 const CardContainer = styled.div`
   display: flex;
   justify-content: center;
+
+  h3 {
+    margin: 0;
+  }
 `;
 
 const Rank = styled.h3`
-  font-size: 30px;
+  font-size: 42px;
   font-family: "Roboto Mono", monospace;
-  padding-right: 30px;
-  padding-left: 30px;
+`;
+
+const Info = styled.div`
+  justify-self: flex-start;
+
+  h2,
+  h4 {
+    padding: 0;
+    margin: 0;
+  }
+
+  h4 {
+    color: #9c9c9c;
+  }
 `;
 
 const LeaderCard = props => {
@@ -52,12 +77,10 @@ const LeaderCard = props => {
     <CardContainer>
       <Card>
         <Rank>#{props.leader.rank}</Rank>
-        <h4>
-          {props.leader.username}
-          <br />
-          <br />
-          Grains of salt: {props.leader.salt_score}
-        </h4>
+        <Info>
+          <h2>{props.leader.username}</h2>
+          <h4>Grains of salt: {props.leader.salt_score}</h4>
+        </Info>
         <StyledLink to={`/user/${props.leader.id}`}>
           <Button>View Comments</Button>
         </StyledLink>
--- saltinator/src/components/leaderboard/Leaderboard.js
+++ saltinator/src/components/leaderboard/Leaderboard.js
@@ -4,11 +4,14 @@ import LeaderCard from "./LeaderCard";
 import { leaderboardData } from "../../utils/mockData";
 import styled from "styled-components";
 
+import colors from "../../styles/colors";
+
 const HeaderContainer = styled.div`
   display: flex;
   margin: auto;
   width: 45vw;
   justify-content: space-between;
+
   @media (max-width: 900px) {
     width: 65vw;
   }
@@ -17,22 +20,29 @@ const HeaderContainer = styled.div`
   }
 `;
 
+const Headers = styled.div`
+  h1,
+  h2 {
+    font-weight: 300;
+  }
+`;
+
 export const Leaderboard = () => {
   return (
-    <div>
+    <Headers>
       <form>
         <h1>Leaderboard</h1>
       </form>
 
-        <HeaderContainer>
-          <h2>Rank</h2>
-          <h2>Username</h2>
-          <h2>Comments</h2>
-        </HeaderContainer>
-      
+      <HeaderContainer>
+        <h2>Rank</h2>
+        <h2>Username</h2>
+        <h2>Comments</h2>
+      </HeaderContainer>
+
       {leaderboardData.map((leader, index) => (
         <LeaderCard leader={leader} key={index} />
       ))}
-    </div>
+    </Headers>
   );
 };

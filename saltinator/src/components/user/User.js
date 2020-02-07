7454693d84fd1789adbda0cdf27dcf8462413727
agohorel <gohorelalex@gmail.com>
2020-02-07 00:10:50 -0500

    cleanup User component

--- saltinator/src/components/user/User.js
+++ saltinator/src/components/user/User.js
@@ -1,33 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
-import { axiosWithAuth } from "../../utils/axiosWithAuth";
 import UserCard from "./UserCard";
 
-const CommentContainer = styled.div`
-  // display: flex;
-  // justify-content: space-between;
-  // align-items: center;
-  // flex-wrap: wrap;
-  // border: 1px solid red;
-  // width: 90vw;
-  // margin: 0 auto;
-`;
+const CommentContainer = styled.div``;
 
 const User = ({ comments }) => {
-  // const [users, setUsers] = useState([]);
-  // useEffect(() => {
-  //   axiosWithAuth()
-  //     .get("users/comments")
-  //     .then(res => {
-  //       setUsers(res.data);
-  //       console.log(res.data);
-  //     })
-  //     .catch(error => {
-  //       console.log("Server Error", error);
-  //     });
-  // }, []);
-
   return (
     <div>
       <form>

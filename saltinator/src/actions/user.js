c4a5a945537a976419d000fd82de58b3bef5535c
agohorel <gohorelalex@gmail.com>
2020-02-06 22:17:38 -0500

    modify actions to work w/ backend

--- saltinator/src/actions/user.js
+++ saltinator/src/actions/user.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { USER_LOADING, USER_SUCCESS, USER_FAILURE } from "./types";
 
 export const fetchUser = username => async dispatch => {
   dispatch({ type: USER_LOADING });
   try {
-    const res = await axios.get(`PRODUCTION_URL_HERE/${username}`);
+    const res = await axiosWithAuth().get(`users/${username}/comments`);
     dispatch({ type: USER_SUCCESS, payload: res.data });
   } catch (err) {
     dispatch({ type: USER_FAILURE, payload: err });

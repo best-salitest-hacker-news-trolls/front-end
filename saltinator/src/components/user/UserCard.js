b80d4d0629a5a509935e7055a6b418036c8b00ba
agohorel <gohorelalex@gmail.com>
2020-02-06 22:08:59 -0500

    fix missing commenter names on user cards

--- saltinator/src/components/user/UserCard.js
+++ saltinator/src/components/user/UserCard.js
@@ -85,7 +85,7 @@ const UserCard = ({
       {pathname !== "/saved" ? (
         <>
           <div>
-            "{comment}"<br />- {username}
+            "{comment}"<br />- {hackerNewsUser}
           </div>
           {isAuthenticated && (
             <Button
